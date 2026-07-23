3f7b47b69bda9d261a953ee8f3f1adc4359a74be
Martin Vasquez <159503418+martinvasquez1@users.noreply.github.com>
2026-07-22 21:22:01 -0400

    Add random number to enemy number

--- characters/enemy_number/enemy_number.gd
+++ characters/enemy_number/enemy_number.gd
@@ -1,5 +1,15 @@
 class_name EnemyNumber
 extends CharacterBody2D
 
+@export var label: Label
+
+var value: int
+
+func _ready() -> void:
+	var random_num: int = randi_range(-9, 9)
+	
+	value = random_num
+	label.text = str(value)
+
 func _on_shoot_timeout() -> void:
 	print("Spawn simple projectile")
