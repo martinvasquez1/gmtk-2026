b02b67847160693f50912df7840ebddf5123be4d
Martin Vasquez <159503418+martinvasquez1@users.noreply.github.com>
2026-07-22 21:35:03 -0400

    Add enemy number movement

--- characters/enemy_number/enemy_number.gd
+++ characters/enemy_number/enemy_number.gd
@@ -1,9 +1,11 @@
 class_name EnemyNumber
 extends CharacterBody2D
 
+@export var speed: float = 20.0
 @export var label: Label
 
 var value: int
+var player_node: Player = null
 
 func _ready() -> void:
 	var random_num: int = randi_range(-9, 9)
@@ -11,5 +13,23 @@ func _ready() -> void:
 	value = random_num
 	label.text = str(value)
 
+func _physics_process(delta: float) -> void:
+	if not player_node:
+		return
+		
+	var target_position: Vector2 = player_node.global_position
+	var direction = self.global_position.direction_to(target_position)
+	
+	velocity = speed * direction
+	move_and_slide()
+
 func _on_shoot_timeout() -> void:
 	print("Spawn simple projectile")
+
+func _on_player_detection_body_entered(body: Node2D) -> void:
+	if body is Player:
+		player_node = body
+
+func _on_player_detection_body_exited(body: Node2D) -> void:
+	if body is Player:
+		player_node = null
