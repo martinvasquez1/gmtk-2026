7e844a097f6492b056e42f03fa9a7c4bf57c16e1
Martin Vasquez <159503418+martinvasquez1@users.noreply.github.com>
2026-07-22 23:13:42 -0400

    Fix unused var warning

--- characters/enemy_number/enemy_number.gd
+++ characters/enemy_number/enemy_number.gd
@@ -13,7 +13,7 @@ func _ready() -> void:
 	value = random_num
 	label.text = str(value)
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	if not player_node:
 		return
 		
